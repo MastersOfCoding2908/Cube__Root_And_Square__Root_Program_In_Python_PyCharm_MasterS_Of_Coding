d88b73fedd63514b3ce688b688a57d63437205f5
root <root@mmg>
2024-02-23 21:32:52 +0530

    Initial commit

---
+++ Project_01.py
@@ -0,0 +1,14 @@
+# Program To Find Square Root And Cube Root Of A Number.
+# Don't Forget To Subscribe Master's Of Coding (@mastersofcoding2907).
+# You Can Get Source Code In Github.
+# https://www.youtube.com/@MastersOfCoding2908
+
+# By Using Math Module
+import math as m
+cb_sq = int(input('Enter A Number To Get Square Root And Cube Root : '))
+print(f'Square Root Of {cb_sq} = {m.sqrt(cb_sq)}')
+print(f'Cube Root Of {cb_sq} = {m.cbrt(cb_sq)}')
+print('Thank You!')
+
+# Thank You For Watching The Video
+# Have A Good Day!
